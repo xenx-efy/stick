f8298c246363d4109ebdc57bb58c1d106ef8c6c2
Xenx <xenx.efy@gmail.com>
2021-05-05 23:29:48 +0300

    Revert "Rename theme folder and configure lando file"
    This reverts commit ba1d2cd1

--- wp-content/themes/wpemerge-theme/resources/scripts/frontend/index.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import config from '@config';
-import './vendor/*.js';
-import '@styles/frontend';
-import '@images/favicon.ico';
-import 'airbnb-browser-shims';
-import './spritesvg';
-
-// Your code goes here ...
